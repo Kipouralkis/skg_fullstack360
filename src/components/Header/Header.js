8764b324cedec4280dd8e6ceb5b73b8923a1a374
Kipouralkis <38573852+Kipouralkis@users.noreply.github.com>
2022-04-07 16:00:02 +0300

    some visual changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 function Header() {
   return (
     <div className="Header">
-      <h1 className='Header-logo'><i class="fas fa-sign-hanging"></i></h1>
+      <h1 className='Header-logo'><i style={{color: "var(--gray-blue)"}} className="fas fa-sign-hanging"></i></h1>
       <h1>Fullstack360</h1>
       <form onSubmit="console.log('You clicked submit.'); return false">
         <button type="submit">Login</button>
