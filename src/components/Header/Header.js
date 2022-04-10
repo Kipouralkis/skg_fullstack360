ea267c54aad521d2b49d71c1f2b1426dcab50cca
Chris <papagrigoriou_c@kalia.gr>
2022-04-10 18:42:17 +0300

    xristoforos

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import "./Header.css";
+import { Outlet, Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="Header">
-      <h1 className='Header-logo'><i style={{color: "var(--gray-blue)"}} className="fas fa-sign-hanging"></i></h1>
+   
+       <Link to="/">
+            <h1 className='Header-logo'><i 
+      style={{color: "var(--gray-blue)"}} 
+      className="fas fa-sign-hanging">
+      </i></h1>
+       </Link>
       <h1>Fullstack360</h1>
       <form onSubmit="console.log('You clicked submit.'); return false">
         <button type="submit">Login</button>
