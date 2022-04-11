085ecd5d5deebfd7d41f5118d7dd9cf1e2d8bd7a
Kipouralkis <38573852+Kipouralkis@users.noreply.github.com>
2022-04-12 01:40:58 +0300

    added button

--- src/ChatbotSearch.js
+++ src/ChatbotSearch.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Chatbot from "react-simple-chatbot";
+import { ThemeProvider } from "styled-components";
+import styled from 'styled-components';
+
+//THEME //
+const theme = {
+  background: "#f5f8fb",
+  headerBgColor: "var(--main-color)",
+  headerFontColor: "#fff",
+  headerFontSize: "15px",
+  botBubbleColor: "var(--green-blue)",
+  botFontColor: "#fff",
+  userBubbleColor: "#fff",
+  userFontColor: "#4a4a4a",
+};
+
+const config ={
+    width: "400px",
+    height: "500px",
+    floating: true,
+  };
+
+//END OF THEME //
 
 class Search extends Component {
   constructor(props) {
@@ -55,74 +77,76 @@ Search.propTypes = {
 Search.defaultProps = {
   steps: undefined,
 };
-
 class SimpleForm extends Component {
   render() {
     return (
-      <Chatbot
-        speechSynthesis={{ enable: true, lang: "en" }}
-        steps={[
-          {
-            id: "1",
-            message: "Hello! What's your name?",
-            trigger: "2",
-          },
-          {
-            id: "2",
-            user: true,
-            trigger: "3",
-          },
-          {
-            id: "3",
-            message:
-              "Hi {previousValue}, nice to meet you! Would you like a house for sale or for rent?",
-            trigger: "to",
-          },
-          {
-            id: "to",
-            options: [
-              { value: "sale", label: "for sale", trigger: "6" },
-              { value: "rent", label: "for rent", trigger: "5" },
-            ],
-          },
-          {
-            id: "5",
-            message: "In what price range are you looking to rent?",
-            trigger: "price",
-          },
-          {
-            id: "6",
-            message: "In what price range are you looking to buy?",
-            trigger: "price",
-          },
-          {
-            id: "price",
-            user: true,
-            trigger: "8",
-          },
-          {
-            id: "8",
-            message: "Where?",
-            trigger: "city",
-          },
-          {
-            id: "city",
-            options: [
-              { value: "Thess", label: "Thessaloniki", trigger: "10" },
-              { value: "Athens", label: "Athens", trigger: "10" },
-            ],
-          },
-          {
-            id: "10",
-            component: <Search />,
-            end: true,
-          },
-          /*  {
+      <ThemeProvider theme={theme}>
+        <Chatbot
+          speechSynthesis={{ enable: true, lang: "en" }}
+          steps={[
+            {
+              id: "1",
+              message: "Hello! What's your name?",
+              trigger: "2",
+            },
+            {
+              id: "2",
+              user: true,
+              trigger: "3",
+            },
+            {
+              id: "3",
+              message:
+                "Hi {previousValue}, nice to meet you! Would you like a house for sale or for rent?",
+              trigger: "to",
+            },
+            {
+              id: "to",
+              options: [
+                { value: "sale", label: "for sale", trigger: "6" },
+                { value: "rent", label: "for rent", trigger: "5" },
+              ],
+            },
+            {
+              id: "5",
+              message: "In what price range are you looking to rent?",
+              trigger: "price",
+            },
+            {
+              id: "6",
+              message: "In what price range are you looking to buy?",
+              trigger: "price",
+            },
+            {
+              id: "price",
+              user: true,
+              trigger: "8",
+            },
+            {
+              id: "8",
+              message: "Where?",
+              trigger: "city",
+            },
+            {
+              id: "city",
+              options: [
+                { value: "Thess", label: "Thessaloniki", trigger: "10" },
+                { value: "Athens", label: "Athens", trigger: "10" },
+              ],
+            },
+            {
+              id: "10",
+              component: <Search />,
+              end: true,
+            },
+            /*  {
             id: '9',
             triger: '9',
           },*/
-        ]}
-      />
+          ]}
+          {...config}
+        />
+      </ThemeProvider>
     );
   }
 }
