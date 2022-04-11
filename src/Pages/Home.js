ee4025e0c0b1f1b2b37e0ef529eebe3917ad8f98
Kipouralkis <38573852+Kipouralkis@users.noreply.github.com>
2022-04-11 22:46:49 +0300

    weee

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,6 +14,8 @@ import { SearchBar } from "../components/SearchBarTemp";
 
 import styled from "styled-components";
 
+import Chatbot from "react-simple-chatbot";
+
 const AppContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -87,6 +89,25 @@ function Home({ data }) {
       </div>
 
       <Footer />
+      <Chatbot
+  steps={[
+    {
+      id: '1',
+      message: 'What is your name?',
+      trigger: '2',
+    },
+    {
+      id: '2',
+      user: true,
+      trigger: '3',
+    },
+    {
+      id: '3',
+      message: 'Hi {previousValue}, nice to meet you!',
+      end: true,
+    },
+  ]}
+/>
     </div>
   );
 }
